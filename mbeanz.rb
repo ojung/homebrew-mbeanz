4d25bccb78cf343941422b608e69669fe4260bd1
Oskar Jung <jung.oskar@gmail.com>
2020-05-07 13:46:30 +0200

    Use idiomatic brew python installation steps

--- mbeanz.rb
+++ mbeanz.rb
@@ -1,15 +1,20 @@
 class Mbeanz < Formula
+  include Language::Python::Virtualenv
+
   desc "A tool for fuzzy finding and invoking mbean operations."
   homepage "https://github.com/ojung/mbeanz"
   url "https://github.com/ojung/mbeanz/releases/download/v1.1.2/mbeanz-1.1.2.tar.xz"
   version "1.1.2"
   sha256 "3f75d26da0a05c68f0ad4c46531abaa49d4d899006fcad03ef373e99cb9a7376"
 
-  depends_on :python
+  depends_on "python"
 
   depends_on "fzf"
 
-  depends_on "requests" => :python
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/f5/4f/280162d4bd4d8aad241a21aecff7a6e46891b905a4341e7ab549ebaf7915/requests-2.23.0.tar.gz"
+    sha256 "b3f43d496c6daba4493e7c431722aeb7dbc6288f52a6e04e7b6023b0247817e6"
+  end
 
   def install
     if (not File.exists?("#{etc}/mbeanz.edn")) then
@@ -22,7 +27,7 @@ class Mbeanz < Formula
       open("#{etc}/mbeanz.edn", "w") { |file| file.write(conf) }
     end
 
-    bin.install Dir["./*"]
+    virtualenv_install_with_resources
   end
 
   def caveats
